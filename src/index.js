be531008facb308c797f1f7225392439c2e407a0
Toleek <tom.0.0.7@yandex.ru>
2021-08-19 22:40:44 +0300

    task: Brackets finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let stack = [];
+
+	for(let i = 0; i < str.length; i++)
+	{
+		let char = str[i];
+		
+		for(let j = 0; j < bracketsConfig.length; j++)
+		{
+			let openChar = bracketsConfig[j][0],
+				closeChar = bracketsConfig[j][1],
+				singleBracket = '';
+				
+			if(openChar === closeChar)
+				singleBracket = openChar;
+				
+			if(char === openChar && (!singleBracket || char !== singleBracket || stack.indexOf(singleBracket) === -1))
+			{
+				stack.push(char);
+			}
+			else if(char === closeChar)
+			{
+				if(!stack.length)
+					return false;
+				
+				let last = stack.pop();
+				if(last !== openChar)
+					return false;
+			}
+		}
+	}
+	
+	if (stack.length !== 0) {
+        return false;
+    }
+
+    return true;
 }
